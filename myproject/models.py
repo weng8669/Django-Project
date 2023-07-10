c2853abf768f574ade4c1605b7878151d3d27620
翁浩銘 Eason Weng <127386850+weng8669@users.noreply.github.com>
2023-07-10 18:43:17 +0800

    update

--- myproject/models.py
+++ myproject/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
     image = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    date = models.CharField(max_length=10, default="2023/06/12")
+    title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
